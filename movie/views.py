c14a3fdbd37f317f62c188bebcf98f81a6db1387
codingMaz <m.balandari.toroghi@gmail.com>
2022-12-12 15:42:26 +0330

    add templates

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Welcome to Home Page</h1>')
+    return render(request, 'home.html', {'name': 'Maz'})
 
 
 def about(request):
-    return HttpResponse("<h1>About Page</h1>")
+    return render(request, 'about.html')
