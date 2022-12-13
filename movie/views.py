20af3da1d5bfab980756ec6c1f222929a8a24208
codingMaz <m.balandari.toroghi@gmail.com>
2022-12-13 13:54:55 +0330

    add movies to the home page

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Movie
 
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    return render(request, 'home.html', {'searchTerm': searchTerm})
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 
 def about(request):
