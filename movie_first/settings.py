6ae0a9923f41c1a37cae5c2b2a2084f191ddc885
codingMaz <m.balandari.toroghi@gmail.com>
2022-12-13 11:58:48 +0330

    add static url

--- movie_first/settings.py
+++ movie_first/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from environs import Env
+import os
 
 env = Env()
 env.read_env()
@@ -128,3 +129,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
