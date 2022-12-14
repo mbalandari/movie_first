f09b9ce2186c077641f1d325f8a0ca28cbc73e81
codingMaz <m.balandari.toroghi@gmail.com>
2022-12-14 14:02:56 +0330

    add base.html

--- movie_first/settings.py
+++ movie_first/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "movie_first.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
