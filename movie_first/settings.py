d266baef109816ac94ec24ba8fa73f314f67761a
codingMaz <m.balandari.toroghi@gmail.com>
2022-12-14 14:25:46 +0330

    update navbar links

--- movie_first/settings.py
+++ movie_first/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "movie_first.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates'],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
