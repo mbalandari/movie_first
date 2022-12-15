0b5bbe051868745df5d98e36373c8d04dd4c97e6
codingMaz <m.balandari.toroghi@gmail.com>
2022-12-15 12:16:43 +0330

    handle password matching error

--- accounts/views.py
+++ accounts/views.py
@@ -16,3 +16,5 @@ def signupaccount(request):
             user.save()
             login(request, user)
             return redirect('home')
+        else:
+            return render(request, 'signupaccount.html', {'form': UserCreationForm, 'error': 'Passwords do not match'})
