79fc10f8b40cfe7268c44d27c4708d069227be98
codingMaz <m.balandari.toroghi@gmail.com>
2022-12-15 14:00:28 +0330

    create review and form

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from .models import Movie
+from .forms import ReviewForm
 
 
 def home(request):
@@ -24,3 +25,19 @@ def signup(request):
 def detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'detail.html', {'movie': movie})
+
+
+def createreview(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    if request.method == 'GET':
+        return render(request, 'createreview.html', {'form': ReviewForm(), 'movie': movie})
+    else:
+        try:
+            form = ReviewForm(request.POST)
+            newReview = form.save(commit=False)
+            newReview.user = request.user
+            newReview.movie = movie
+            newReview.save()
+            return redirect('detail', newReview.movie.id)
+        except ValueError:
+            return render(request, 'createreview.html', {'form': ReviewForm(), 'error': 'bad data passed in'})
