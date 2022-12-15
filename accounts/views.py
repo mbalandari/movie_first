6539cc20a1417068ddca2e3ffb89d08c4b4b703e
codingMaz <m.balandari.toroghi@gmail.com>
2022-12-15 12:35:17 +0330

    use new form in the view

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreateForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.shortcuts import redirect
@@ -8,7 +9,7 @@ from django.db import IntegrityError
 
 def signupaccount(request):
     if request.method == 'GET':
-        return render(request, 'signupaccount.html', {'form': UserCreationForm})
+        return render(request, 'signupaccount.html', {'form': UserCreateForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
@@ -21,8 +22,8 @@ def signupaccount(request):
             except IntegrityError:
                 return render(
                     request, 'signupaccount.html',
-                    {'form': UserCreationForm,
+                    {'form': UserCreateForm,
                         'error': 'Username already taken. Choose new username.'}
                 )
         else:
-            return render(request, 'signupaccount.html', {'form': UserCreationForm, 'error': 'Passwords do not match'})
+            return render(request, 'signupaccount.html', {'form': UserCreateForm, 'error': 'Passwords do not match'})
